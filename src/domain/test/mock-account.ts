58401f9ea77ff6e1030fa835ff434244a0e97397
Celso Junior <clsjunnior@live.com>
2022-08-15 21:31:07 -0300

    test: ensure remoteAuthentication returns Account on success

--- src/domain/test/mock-account.ts
+++ src/domain/test/mock-account.ts
@@ -1,9 +1,16 @@
 import { AuthenticationParams } from '@/domain/usecases/authentication'
 import { faker } from '@faker-js/faker'
+import { AccountModel } from '../models/account-model'
 
 export const mockAuthentication = (): AuthenticationParams => {
   return {
     email: faker.internet.email(),
     password: faker.internet.password()
+  }
+}
+
+export const mockAccountModel = (): AccountModel => {
+  return {
+    accessToken: faker.random.alphaNumeric()
   }
 }
