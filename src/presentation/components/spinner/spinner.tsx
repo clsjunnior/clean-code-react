14318eefb6394c9bad232cc99eb8c723f37b97cd
Celso Junior <clsjunnior@live.com>
2022-08-20 11:30:01 -0300

    refactor: move form status  to a separate file

---
+++ src/presentation/components/spinner/spinner.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import Styles from './spinner-styles.scss'
+
+type Props = React.HTMLAttributes<HTMLDivElement>
+
+/* eslint-disable react/prop-types */
+const Spinner: React.FC<Props> = (props) => {
+  return (
+    <div {...props} className={[Styles.spinner, props.className].join(' ')}>
+      <div />
+      <div />
+      <div />
+      <div />
+    </div>
+  )
+}
+
+export default Spinner
--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -4,6 +4,7 @@ import Styles from './login-styles.scss'
 import Footer from '@/presentation/components/footer/footer'
 import LoginHeader from '@/presentation/components/login-header/login-header'
 import Input from '@/presentation/components/input/input'
+import FormStatus from '@/presentation/components/form-status/form-status'
 
 const Login: React.FC = () => {
   return (
@@ -17,9 +18,7 @@ const Login: React.FC = () => {
           Entrar
         </button>
         <span className={Styles.link}>Criar conta</span>
-        <div className={Styles.errorWrap}>
-          <span className={Styles.error}>Usuário ou senha inválidos</span>
-        </div>
+        <FormStatus />
       </form>
       <Footer />
     </div>
