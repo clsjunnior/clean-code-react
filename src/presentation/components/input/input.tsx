e8a7a6f219d4fac89ed39cf2842fc3c5a1f3259d
Celso Junior <clsjunnior@live.com>
2022-08-21 15:36:26 -0300

    feat: block autocomplete from chrome

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -9,9 +9,13 @@ HTMLInputElement
 >
 
 const Input: React.FC<InputProps> = (props: InputProps) => {
+  const enableInput = (event: React.FocusEvent<HTMLInputElement>): void => {
+    event.target.readOnly = false
+  }
+
   return (
     <div className={Styles.inputWrap}>
-      <input {...props} />
+      <input {...props} readOnly onFocus={enableInput} />
       <div className={Styles.status}>
         <ErrorIcon />
       </div>
