39b76fa355ed71a019a83dad823ab384b50fe883
Celso Junior <clsjunnior@live.com>
2022-08-23 23:01:29 -0300

    test: ensure login calls validation with correct password

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -60,4 +60,13 @@ describe('Login component', () => {
       email: 'any_email'
     })
   })
+
+  test('Should call validation with correct password', () => {
+    const { sut, validationSpy } = makeSut()
+    const passwordInput = sut.getByTestId('password')
+    fireEvent.input(passwordInput, { target: { value: 'any_password' } })
+    expect(validationSpy.input).toEqual({
+      password: 'any_password'
+    })
+  })
 })
