82a274ae8ef37e08141b456c8b564be1d43dcc60
Celso Junior <clsjunnior@live.com>
2022-08-23 22:31:40 -0300

    refactor: move SUT creation to a factory method

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -1,25 +1,37 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, RenderResult } from '@testing-library/react'
 import Login from './login'
 import { ErrorIcon } from '@/presentation/components/input/errorIcon'
 
+type SutTypes = {
+  sut: RenderResult
+}
+// factory
+const makeSut = (): SutTypes => {
+  const sut = render(<Login />)
+
+  return {
+    sut
+  }
+}
+
 describe('Login component', () => {
   test('Should start with initial state', () => {
-    const { getByTestId } = render(<Login />)
-    const errorWrap = getByTestId('error-wrap')
+    const { sut } = makeSut()
+    const errorWrap = sut.getByTestId('error-wrap')
     expect(errorWrap.childElementCount).toBe(0)
 
-    const submitButton = getByTestId('submit') as HTMLButtonElement
+    const submitButton = sut.getByTestId('submit') as HTMLButtonElement
     expect(submitButton.disabled).toBe(true)
 
-    const emailStatus = getByTestId('email-status') as HTMLSpanElement
+    const emailStatus = sut.getByTestId('email-status') as HTMLSpanElement
     expect(emailStatus.title).toBe('Campo obrigatório')
 
     const { container } = render(<ErrorIcon />)
 
     expect(emailStatus.innerHTML).toBe(container.querySelector('svg').outerHTML)
 
-    const passwordStatus = getByTestId('password-status') as HTMLDivElement
+    const passwordStatus = sut.getByTestId('password-status') as HTMLDivElement
     expect(passwordStatus.title).toBe('Campo obrigatório')
     expect(passwordStatus.innerHTML).toBe(container.querySelector('svg').outerHTML)
   })
